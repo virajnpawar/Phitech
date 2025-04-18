66676354f6d932c3e1f5760e368247d66037fb21
virajnpawar <virajpawar2002@gmail.com>
2025-04-18 11:58:28 +0530

    v5

--- js/theme.js
+++ js/theme.js
@@ -490,15 +490,6 @@
 })(jQuery)
 
 
-
-
-
-
-
-
-
-
-
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee-content");
